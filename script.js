0c327844c30820bb312801a202cace2ea79ff723
kyrylsherstobaiev <novemberua@gmail.com>
2021-04-27 17:16:54 +0300

    min changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let inputarea = document.querySelector('input[data-numeric="numeric"]');
 let inputArray = [];
 
@@ -8,7 +7,7 @@ let getArrayNumbers = () => new Promise((resolve, reject) => {
 
 getArrayNumbers()
 	.then(
-		function () {
+		() => {
 			inputarea.focus();
 			inputarea.addEventListener('keypress', (e) => {
 				if (e.key === 'Enter') {
@@ -21,19 +20,21 @@ getArrayNumbers()
 		}
 	)
 	.then(
-		function () {
+		() => {
 			setTimeout(() => {
 				inputarea.setAttribute("readonly", 1);
 				inputarea.value = '';
 				console.log(inputArray);
 			}, 5000);
-		})
+		}
+	)
 	.catch(
-		function () {
+		() => {
 			inputarea.setAttribute("readonly", 1);
 			console.log('Action was cancelled by user');
 		}
 	)
+
 
 
 
